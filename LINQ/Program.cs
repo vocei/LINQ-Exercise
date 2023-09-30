472e4fd34e458afbabd0768876d24ea0a0494f86
Clifford Myrtil <cliffordhamyrtil@gmail.com>
2023-09-30 16:07:59 -0400

    Finished Exersize 2 LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+                List<string> myGame = new List<string>() { "Mario", "Sonic", "Call of Duty", "Payday 3" };
+                {
+                    var orderedMyGame = myGame.OrderByDescending(x => x.Length);
+
+                    foreach (var games in orderedMyGame)
+
+                    {
+                        Console.WriteLine(games);
+                    }
+                }
+            }
         }
     }
-}
+
